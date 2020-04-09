fcbb17d8276a766eb56df13c84bcb612e21fc1fd
barak <58682332+bgavyus@users.noreply.github.com>
2020-04-09 19:58:53 +0300

    private set in StatefulMediaRecorder

--- app/src/main/java/io/github/bgavyus/splash/recording/StatefulMediaRecorder.kt
+++ app/src/main/java/io/github/bgavyus/splash/recording/StatefulMediaRecorder.kt
@@ -3,18 +3,17 @@ package io.github.bgavyus.splash.recording
 import android.media.MediaRecorder
 
 open class StatefulMediaRecorder : MediaRecorder() {
-    private var _state = RecorderState.Initial
+	var state = RecorderState.Initial
+		private set
+
     private var onErrorListener: OnErrorListener? = null
 
     init {
-        MediaRecorder()
         setOnErrorListener { mr, what, extra ->
-            _state = RecorderState.Error
+            state = RecorderState.Error
             onErrorListener?.onError(mr, what, extra)
         }
     }
-
-    val state get() = _state
 
     override fun setOnErrorListener(l: OnErrorListener?) {
         onErrorListener = l
@@ -22,51 +21,51 @@ open class StatefulMediaRecorder : MediaRecorder() {
 
     override fun setAudioSource(audioSource: Int) {
         super.setAudioSource(audioSource)
-        _state = RecorderState.Initialized
+        state = RecorderState.Initialized
     }
 
     override fun setVideoSource(video_source: Int) {
         super.setVideoSource(video_source)
-        _state = RecorderState.Initialized
+        state = RecorderState.Initialized
     }
 
     override fun setOutputFormat(output_format: Int) {
         super.setOutputFormat(output_format)
-        _state = RecorderState.DataSourceConfigured
+        state = RecorderState.DataSourceConfigured
     }
 
     override fun prepare() {
         super.prepare()
-        _state = RecorderState.Prepared
+        state = RecorderState.Prepared
     }
 
     override fun start() {
         super.start()
-        _state = RecorderState.Recording
+        state = RecorderState.Recording
     }
 
     override fun stop() {
         super.stop()
-        _state = RecorderState.Initial
+        state = RecorderState.Initial
     }
 
     override fun reset() {
         super.reset()
-        _state = RecorderState.Initial
+        state = RecorderState.Initial
     }
 
     override fun release() {
         super.release()
-        _state = RecorderState.Released
+        state = RecorderState.Released
     }
 
     override fun pause() {
         super.pause()
-        _state = RecorderState.Paused
+        state = RecorderState.Paused
     }
 
     override fun resume() {
         super.resume()
-        _state = RecorderState.Recording
+        state = RecorderState.Recording
     }
 }
