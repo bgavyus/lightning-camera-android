ad900cd7b903952f4345e902586e6d13bdc9ed04
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-09-26 01:22:00 +0300

    Removed unused string

--- app/src/main/java/io/github/bgavyus/splash/common/ApplicationModule.kt
+++ app/src/main/java/io/github/bgavyus/splash/common/ApplicationModule.kt
@@ -14,6 +14,7 @@ import java.time.Clock
 @Module
 object ApplicationModule {
     @Provides
+    // TODO: Replace with API 24 supported clock
     fun provideClock(): Clock = Clock.systemDefaultZone()
 
     @Provides
