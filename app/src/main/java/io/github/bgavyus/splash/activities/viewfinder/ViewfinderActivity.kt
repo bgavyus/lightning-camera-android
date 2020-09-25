62efa5e7fed69d37af74a0f455cb8d21d4c10dd6
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-09-26 00:57:30 +0300

    Support edge-to-edge view

--- app/src/main/java/io/github/bgavyus/splash/activities/viewfinder/ViewfinderActivity.kt
+++ app/src/main/java/io/github/bgavyus/splash/activities/viewfinder/ViewfinderActivity.kt
@@ -46,7 +46,9 @@ class ViewfinderActivity : FragmentActivity(), TextureView.SurfaceTextureListene
 
     private fun enterFullScreen() {
         window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 
     private fun inflateView() {
