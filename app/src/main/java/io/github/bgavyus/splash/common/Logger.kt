bd2eb4e7bc314e4e9d1d83e299273554db202024
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-09-27 15:36:11 +0300

    Simplified some logger logic

--- app/src/main/java/io/github/bgavyus/splash/common/Logger.kt
+++ app/src/main/java/io/github/bgavyus/splash/common/Logger.kt
@@ -9,20 +9,20 @@ object Logger {
     fun debug(message: String) = log(Log.DEBUG, message)
     fun info(message: String) = log(Log.INFO, message)
     fun warn(message: String) = log(Log.WARN, message)
-    fun warn(message: String, throwable: Throwable) = log(Log.WARN, message, throwable)
+    fun warn(message: String, throwable: Throwable) = log(Log.WARN, formatMessage(message, throwable))
     fun error(message: String) = log(Log.ERROR, message)
-    fun error(message: String, throwable: Throwable) = log(Log.ERROR, message, throwable)
+    fun error(message: String, throwable: Throwable) = log(Log.ERROR, formatMessage(message, throwable))
 
-    private fun log(priority: Int, message: String, throwable: Throwable? = null) {
-        Log.println(priority, getTag(), resolveMessage(message, throwable))
+    private fun log(priority: Int, message: String) {
+        Log.println(priority, tag(), message)
     }
 
-    private fun getTag(): String {
+    private fun tag(): String {
         val element = Throwable().stackTrace[4]
         val result = tagRegex.find(element.className) ?: throw RuntimeException()
         return result.groupValues[1]
     }
 
-    private fun resolveMessage(message: String, throwable: Throwable? = null) =
-        if (throwable == null) message else "$message\n${Log.getStackTraceString(throwable)}"
+    private fun formatMessage(message: String, throwable: Throwable) =
+        "$message\n${Log.getStackTraceString(throwable)}"
 }
