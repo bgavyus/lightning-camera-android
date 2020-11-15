0c8a675bd70e02ca7572582a46846dda8ff1ef76
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-11-16 00:02:55 +0200

    Fixed logger tag and removed unused function

--- app/src/main/java/io/github/bgavyus/splash/common/Logger.kt
+++ app/src/main/java/io/github/bgavyus/splash/common/Logger.kt
@@ -10,7 +10,6 @@ object Logger {
     fun info(message: String) = log(Log.INFO, message)
     fun warn(message: String) = log(Log.WARN, message)
     fun warn(message: String, throwable: Throwable) = log(Log.WARN, concat(message, throwable))
-    fun error(message: String) = log(Log.ERROR, message)
     fun error(message: String, throwable: Throwable) = log(Log.ERROR, concat(message, throwable))
 
     private fun log(priority: Int, message: String) {
@@ -18,7 +17,7 @@ object Logger {
     }
 
     private fun tag(): String {
-        val element = Throwable().stackTrace[4]
+        val element = Throwable().stackTrace[3]
         val result = tagRegex.find(element.className) ?: throw RuntimeException()
         return result.groupValues[1]
     }
