2d9bff99df45830b312a9ca60caf52768afbdd4c
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-12-19 15:58:50 +0200

    Added crashlytics custom logs

--- app/src/main/java/io/github/bgavyus/lightningcamera/common/Logger.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/common/Logger.kt
@@ -1,6 +1,7 @@
 package io.github.bgavyus.lightningcamera.common
 
 import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.github.bgavyus.lightningcamera.BuildConfig
 
 object Logger {
@@ -9,12 +10,21 @@ object Logger {
     fun debug(message: String) = log(Log.DEBUG, message)
     fun info(message: String) = log(Log.INFO, message)
     fun warn(message: String) = log(Log.WARN, message)
-    fun error(message: String, throwable: Throwable) = log(Log.ERROR, concat(message, throwable))
+
+    fun error(message: String, throwable: Throwable) {
+        log(Log.ERROR, concat(message, throwable))
+        FirebaseCrashlytics.getInstance().recordException(throwable)
+    }
 
     private fun log(priority: Int, message: String) {
+        val tag = tag()
+
         if (BuildConfig.DEBUG) {
-            Log.println(priority, tag(), message)
+            Log.println(priority, "${BuildConfig.APPLICATION_ID}.$tag", message)
         }
+
+        val prioritySymbol = prioritySymbol(priority)
+        FirebaseCrashlytics.getInstance().log("$prioritySymbol/$tag: $message")
     }
 
     private fun tag(): String {
@@ -25,4 +35,14 @@ object Logger {
 
     private fun concat(message: String, throwable: Throwable) =
         "$message\n${Log.getStackTraceString(throwable)}"
+
+    private fun prioritySymbol(priority: Int) = when (priority) {
+        Log.VERBOSE -> 'V'
+        Log.DEBUG -> 'D'
+        Log.INFO -> 'I'
+        Log.WARN -> 'W'
+        Log.ERROR -> 'E'
+        Log.ASSERT -> 'A'
+        else -> '?'
+    }
 }
