df500fdea44f2ea2885e2a6e9fb83cf762216c8d
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2020-12-28 01:58:52 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdkVersion(24)
         targetSdkVersion(30)
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.1.0"
     }
 
     buildFeatures {
