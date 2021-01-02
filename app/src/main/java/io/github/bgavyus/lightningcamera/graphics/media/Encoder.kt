4e8f0a12bf211b7a0b75dc407ad3f6afae433352
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-01-02 15:01:00 +0200

    Created MediaCodec flow adapter

--- app/src/main/java/io/github/bgavyus/lightningcamera/graphics/media/Encoder.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/graphics/media/Encoder.kt
@@ -2,89 +2,53 @@ package io.github.bgavyus.lightningcamera.graphics.media
 
 import android.media.MediaCodec
 import android.media.MediaFormat
+import android.os.Handler
 import android.view.Surface
 import io.github.bgavyus.lightningcamera.common.DeferScope
 import io.github.bgavyus.lightningcamera.common.Logger
 import io.github.bgavyus.lightningcamera.common.SingleThreadHandler
+import io.github.bgavyus.lightningcamera.common.extensions.cancel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.android.asCoroutineDispatcher
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class Encoder(format: MediaFormat) : DeferScope() {
     private val handler = SingleThreadHandler(javaClass.simpleName)
         .apply { defer(::close) }
 
-    var listener: EncoderListener? = null
+    private val dispatcher = handler.asCoroutineDispatcher(javaClass.simpleName)
 
+    private val scope = CoroutineScope(dispatcher)
+        .apply { defer(::cancel) }
+
+    private val codec: MediaCodec
     val surface: Surface
+    var listener: EncoderListener? = null
 
     init {
         val mimeType = format.getString(MediaFormat.KEY_MIME)
             ?: throw IllegalArgumentException()
 
-        val callback = object : MediaCodec.Callback() {
-            override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
-                Logger.debug("Format available")
-                listener?.onFormatAvailable(format)
-            }
+        codec = MediaCodec.createEncoderByType(mimeType).apply {
+            defer(::release)
 
-            override fun onOutputBufferAvailable(
-                codec: MediaCodec,
-                index: Int,
-                info: MediaCodec.BufferInfo
-            ) {
-                try {
-                    if (info.isCodecConfig) {
-                        Logger.debug("Got codec config")
-                        return
-                    }
+            encoderEvents(handler).onEach {
+                when (it) {
+                    is EncoderEvent.OutputFormatChanged ->
+                        onOutputFormatChanged(it.format)
 
-                    if (info.isEndOfStream) {
-                        Logger.warn("Got end of stream")
-                        return
-                    }
-
-                    if (info.size == 0) {
-                        Logger.warn("Got empty buffer")
-                        return
-                    }
-
-                    try {
-                        val buffer = codec.getOutputBuffer(index)
-
-                        if (buffer == null) {
-                            Logger.warn("Got null buffer")
-                            return
-                        }
-
-                        listener?.onBufferAvailable(buffer, info)
-                    } catch (_: IllegalStateException) {
-                        Logger.debug("Ignoring buffer after release")
-                    }
-                } finally {
-                    try {
-                        codec.releaseOutputBuffer(index, false)
-                    } catch (_: IllegalStateException) {
-                        Logger.debug("Ignoring buffer after release")
-                    }
+                    is EncoderEvent.OutputBufferAvailable -> onOutputBufferAvailable(it.index,
+                        it.info)
                 }
             }
+                .launchIn(scope)
 
-            override fun onError(codec: MediaCodec, error: MediaCodec.CodecException) {
-                Logger.error("Media codec error", error)
-                listener?.onEncoderError(error)
-            }
-
-            override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {}
-        }
-
-        MediaCodec.createEncoderByType(mimeType).run {
-            defer(::release)
-            setCallback(callback, handler)
-
-            configure(
-                format,
-                null,
-                null,
-                MediaCodec.CONFIGURE_FLAG_ENCODE
-            )
+            configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
 
             surface = createInputSurface()
                 .apply { defer(::release) }
@@ -94,6 +58,76 @@ class Encoder(format: MediaFormat) : DeferScope() {
             defer(::flush)
         }
     }
+
+    private fun onOutputFormatChanged(format: MediaFormat) {
+        Logger.debug("Format available")
+        listener?.onFormatAvailable(format)
+    }
+
+    private fun onOutputBufferAvailable(index: Int, info: MediaCodec.BufferInfo) {
+        try {
+            if (info.isCodecConfig) {
+                Logger.debug("Got codec config")
+                return
+            }
+
+            if (info.isEndOfStream) {
+                Logger.warn("Got end of stream")
+                return
+            }
+
+            if (info.size == 0) {
+                Logger.warn("Got empty buffer")
+                return
+            }
+
+            try {
+                val buffer = codec.getOutputBuffer(index)
+
+                if (buffer == null) {
+                    Logger.warn("Got null buffer")
+                    return
+                }
+
+                listener?.onBufferAvailable(buffer, info)
+            } catch (_: IllegalStateException) {
+                Logger.debug("Ignoring buffer after release")
+            }
+        } finally {
+            try {
+                codec.releaseOutputBuffer(index, false)
+            } catch (_: IllegalStateException) {
+                Logger.debug("Ignoring buffer after release")
+            }
+        }
+    }
+}
+
+private sealed class EncoderEvent {
+    data class OutputFormatChanged(val format: MediaFormat) :
+        EncoderEvent()
+
+    data class OutputBufferAvailable(val index: Int, val info: MediaCodec.BufferInfo) :
+        EncoderEvent()
+}
+
+private fun MediaCodec.encoderEvents(handler: Handler) = callbackFlow<EncoderEvent> {
+    val callback = object : MediaCodec.Callback() {
+        override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) =
+            sendBlocking(EncoderEvent.OutputFormatChanged(format))
+
+        override fun onOutputBufferAvailable(
+            codec: MediaCodec,
+            index: Int,
+            info: MediaCodec.BufferInfo,
+        ) = sendBlocking(EncoderEvent.OutputBufferAvailable(index, info))
+
+        override fun onError(codec: MediaCodec, error: MediaCodec.CodecException) = cancel(error)
+        override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {}
+    }
+
+    setCallback(callback, handler)
+    awaitClose { setCallback(null) }
 }
 
 val MediaCodec.BufferInfo.isCodecConfig get() = flags.isSet(MediaCodec.BUFFER_FLAG_CODEC_CONFIG)
