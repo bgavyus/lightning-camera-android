ff83751200e72c3ef09d52e2912d9290abe64d4d
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-01-06 23:49:22 +0200

    Fix early bind in recorder

--- app/src/main/java/io/github/bgavyus/lightningcamera/graphics/media/Recorder.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/graphics/media/Recorder.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import java.nio.ByteBuffer
@@ -69,6 +70,7 @@ class Recorder(
             .also { defer(it::close) }
 
         encoder.format
+            .filterNotNull()
             .onEach {
                 this.format = it
                 bind()
