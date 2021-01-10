a4d7336eaa88bd8c7691debe419e12b2ef07b22e
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-01-10 19:10:49 +0200

    Reified flow extensions

--- app/src/main/java/io/github/bgavyus/lightningcamera/common/extensions/FlowExtensions.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/common/extensions/FlowExtensions.kt
@@ -8,9 +8,9 @@ fun <T> Flow<T>.callOnEach(action: (T) -> Unit) = onEach { action(it) }
 
 fun <T> Flow<T>.reflectTo(other: MutableStateFlow<T>) = onEach { other.value = it }
 
-fun Flow<Boolean>.onToggle(on: () -> Any?, off: () -> Any?) =
+fun Flow<Boolean>.onToggle(on: () -> Unit, off: () -> Unit) =
     distinctUntilChanged().onEach { if (it) on() else off() }
 
 infix fun Flow<Boolean>.and(flow: Flow<Boolean>) = combine(flow) { a, b -> a && b }
 
-fun CoroutineScope.launchAll(vararg flows: Flow<Any?>) = flows.forEach { it.launchIn(this) }
+fun CoroutineScope.launchAll(vararg flows: Flow<*>) = flows.forEach { it.launchIn(this) }
