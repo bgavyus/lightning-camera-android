edf5aa286a099ed72abc06a9a0c55b6caa974db2
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-01-17 13:16:42 +0200

    Support regular speed capture

--- app/src/main/java/io/github/bgavyus/lightningcamera/capture/CameraMetadataProvider.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/capture/CameraMetadataProvider.kt
@@ -1,9 +1,12 @@
 package io.github.bgavyus.lightningcamera.capture
 
 import android.content.Context
+import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CameraMetadata.REQUEST_AVAILABLE_CAPABILITIES_CONSTRAINED_HIGH_SPEED_VIDEO
+import android.util.Range
+import android.util.Size
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.bgavyus.lightningcamera.common.Logger
 import io.github.bgavyus.lightningcamera.common.Rotation
@@ -19,36 +22,74 @@ class CameraMetadataProvider @Inject constructor(
     suspend fun collect() = withContext(Dispatchers.IO) {
         val manager = context.systemService<CameraManager>()
 
-        val (id, characteristics) = manager.cameraIdList
-            .asSequence()
+        val pairs = manager.cameraIdList
             .map { id -> Pair(id, manager.getCameraCharacteristics(id)) }
-            .find { (_, characteristics) ->
-                characteristics[CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES]
-                    ?.contains(CameraMetadata.REQUEST_AVAILABLE_CAPABILITIES_CONSTRAINED_HIGH_SPEED_VIDEO)
-                    ?: false
-            } ?: throw HighSpeedCameraUnavailableException()
 
-        val orientation = characteristics[CameraCharacteristics.SENSOR_ORIENTATION]
-            ?.let { Rotation.fromDegrees(it) }
-            ?: throw RuntimeException()
+        (collectHighSpeed(pairs) ?: collectRegularSpeed(pairs))
+            .also { Logger.info("Metadata collected: $it") }
+    }
 
-        val configMap = characteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]
-            ?: throw RuntimeException()
+    private fun collectHighSpeed(cameras: Iterable<Pair<String, CameraCharacteristics>>): CameraMetadata? {
+        val (id, characteristics) = cameras
+            .find { (_, characteristics) -> characteristics.supportsHighSpeed }
+            ?: return null
 
-        val (framesPerSecond, frameSize) = configMap.highSpeedVideoFpsRanges
+        val streamConfigurationMap = characteristics.streamConfigurationMap
+
+        val (framesPerSecond, frameSize) = streamConfigurationMap.highSpeedVideoFpsRanges
             .asSequence()
-            .filter { it.lower == it.upper }
+            .filter(Range<Int>::isSingular)
             .flatMap { fpsRange ->
-                configMap.getHighSpeedVideoSizesFor(fpsRange)
+                streamConfigurationMap.getHighSpeedVideoSizesFor(fpsRange)
                     .asSequence()
                     .map { frameSize -> Pair(fpsRange.upper, frameSize) }
             }
-            .maxByOrNull { (framesPerSecond, frameSize) -> framesPerSecond * frameSize.area }
-            ?: throw RuntimeException()
+            .requireMaxBy { (framesPerSecond, frameSize) -> framesPerSecond * frameSize.area }
 
-        Logger.info("Metadata collected: orientation: $orientation, frames/second: $framesPerSecond, frame Size: $frameSize")
-        CameraMetadata(id, orientation, framesPerSecond, frameSize)
+        return CameraMetadata(id, characteristics.orientation, framesPerSecond, frameSize)
+    }
+
+    private fun collectRegularSpeed(cameras: Iterable<Pair<String, CameraCharacteristics>>): CameraMetadata {
+        val (id, characteristics, framesPerSecond) = cameras
+            .asSequence()
+            .map { (id, characteristics) ->
+                val framesPerSecond = characteristics.fpsRanges
+                    .asSequence()
+                    .filter(Range<Int>::isSingular)
+                    .maxOf(Range<Int>::getUpper)
+
+                Triple(id, characteristics, framesPerSecond)
+            }
+            .requireMaxBy { (_, _, framesPerSecond) -> framesPerSecond }
+
+        val frameSize = characteristics.streamConfigurationMap
+            .getOutputSizes(SurfaceTexture::class.java)
+            .asSequence()
+            .requireMaxBy(Size::area)
+
+        return CameraMetadata(id, characteristics.orientation, framesPerSecond, frameSize)
     }
 }
 
-class HighSpeedCameraUnavailableException : Exception()
+private val CameraCharacteristics.supportsHighSpeed
+    get() = REQUEST_AVAILABLE_CAPABILITIES_CONSTRAINED_HIGH_SPEED_VIDEO in capabilities
+
+private val CameraCharacteristics.capabilities
+    get() = requireGet(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES)
+
+private val CameraCharacteristics.streamConfigurationMap
+    get() = requireGet(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+
+private val CameraCharacteristics.orientation
+    get() = Rotation.fromDegrees(requireGet(CameraCharacteristics.SENSOR_ORIENTATION))
+
+private val CameraCharacteristics.fpsRanges
+    get() = requireGet(CameraCharacteristics.CONTROL_AE_AVAILABLE_TARGET_FPS_RANGES)
+
+private fun <T> CameraCharacteristics.requireGet(key: CameraCharacteristics.Key<T>) =
+    get(key) ?: throw RuntimeException()
+
+private fun <T, R : Comparable<R>> Sequence<T>.requireMaxBy(selector: (T) -> R) =
+    maxByOrNull(selector) ?: throw RuntimeException()
+
+private val <T : Comparable<T>> Range<T>.isSingular get() = lower == upper
--- app/src/main/java/io/github/bgavyus/lightningcamera/capture/CameraSessionFactory.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/capture/CameraSessionFactory.kt
@@ -17,29 +17,46 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 class CameraSessionFactory : DeferScope() {
+    companion object {
+        const val infinityFocus = 0f
+        const val highSpeedMinimalFps = 120
+    }
+
     private val handler = SingleThreadHandler(javaClass.simpleName)
         .apply { defer(::close) }
 
-    suspend fun create(device: CameraDevice, surfaces: List<Surface>, framesPerSecond: Int) =
-        device.createConstrainedHighSpeedCaptureSession(surfaces, handler).apply {
-            val captureRequest = device.createCaptureRequest(CameraDevice.TEMPLATE_RECORD).apply {
-                set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, framesPerSecond.toRange())
-                set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF)
-                set(CaptureRequest.LENS_FOCUS_DISTANCE, 0f)
-                surfaces.forEach { addTarget(it) }
-            }.build()
+    suspend fun create(
+        device: CameraDevice,
+        surfaces: List<Surface>,
+        framesPerSecond: Int,
+    ): CameraCaptureSession {
+        val captureRequest = device.createCaptureRequest(CameraDevice.TEMPLATE_RECORD).apply {
+            set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, framesPerSecond.toRange())
+            set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF)
+            set(CaptureRequest.LENS_FOCUS_DISTANCE, infinityFocus)
+            surfaces.forEach(::addTarget)
+        }.build()
 
-            setRepeatingBurst(createHighSpeedRequestList(captureRequest), null, handler)
+        val isHighSpeed = framesPerSecond >= highSpeedMinimalFps
+
+        return device.createCaptureSession(isHighSpeed, surfaces, handler).apply {
+            val requests = (this as? CameraConstrainedHighSpeedCaptureSession)
+                ?.createHighSpeedRequestList(captureRequest)
+                ?: listOf(captureRequest)
+
+            setRepeatingBurst(requests, null, handler)
         }
+    }
 }
 
-private suspend fun CameraDevice.createConstrainedHighSpeedCaptureSession(
+private suspend fun CameraDevice.createCaptureSession(
+    isHighSpeed: Boolean,
     surfaces: List<Surface>,
     handler: Handler,
-): CameraConstrainedHighSpeedCaptureSession = suspendCoroutine { continuation ->
+): CameraCaptureSession = suspendCoroutine { continuation ->
     val callback = object : CameraCaptureSession.StateCallback() {
         override fun onConfigured(session: CameraCaptureSession) {
-            continuation.resume(session as CameraConstrainedHighSpeedCaptureSession)
+            continuation.resume(session)
         }
 
         override fun onConfigureFailed(session: CameraCaptureSession) {
@@ -48,9 +65,14 @@ private suspend fun CameraDevice.createConstrainedHighSpeedCaptureSession(
     }
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        val sessionType = when (isHighSpeed) {
+            true -> SessionConfiguration.SESSION_HIGH_SPEED
+            false -> SessionConfiguration.SESSION_REGULAR
+        }
+
         val sessionConfig = SessionConfiguration(
-            SessionConfiguration.SESSION_HIGH_SPEED,
-            surfaces.map { OutputConfiguration(it) },
+            sessionType,
+            surfaces.map(::OutputConfiguration),
             handler::post,
             callback,
         )
@@ -58,11 +80,10 @@ private suspend fun CameraDevice.createConstrainedHighSpeedCaptureSession(
         createCaptureSession(sessionConfig)
     } else {
         @Suppress("DEPRECATION")
-        createConstrainedHighSpeedCaptureSession(
-            surfaces,
-            callback,
-            handler,
-        )
+        when (isHighSpeed) {
+            true -> createConstrainedHighSpeedCaptureSession(surfaces, callback, handler)
+            false -> createCaptureSession(surfaces, callback, handler)
+        }
     }
 }
 
