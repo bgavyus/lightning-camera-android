cc06fc76bdbea4c7a8ba7dd5b408b39f786dc8c8
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-01-28 00:38:44 +0200

    Enable StrictMode

--- app/src/main/java/io/github/bgavyus/lightningcamera/common/Application.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/common/Application.kt
@@ -1,7 +1,25 @@
 package io.github.bgavyus.lightningcamera.common
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
+import io.github.bgavyus.lightningcamera.BuildConfig
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            enableStrictMode()
+        }
+    }
+
+    private fun enableStrictMode() =
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+}
