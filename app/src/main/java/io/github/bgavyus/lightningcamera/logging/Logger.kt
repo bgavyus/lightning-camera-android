158ffcc857ccb6a1be5ab80dee58104bdce13269
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-03-13 16:40:51 +0200

    Logger: deduce tag from filename instead of class name

--- app/src/main/java/io/github/bgavyus/lightningcamera/logging/Logger.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/logging/Logger.kt
@@ -3,13 +3,11 @@ package io.github.bgavyus.lightningcamera.logging
 import io.github.bgavyus.lightningcamera.extensions.callerStackTraceElement
 
 object Logger {
-    private val tagExtractor = LoggerTagExtractor()
     private val printer = PrinterProvider.get()
 
     fun log(message: String) {
+        val tag = Throwable().callerStackTraceElement.fileName.substringBeforeLast(".")
         val threadName = Thread.currentThread().name
-        val className = Throwable().callerStackTraceElement.className
-        val tag = tagExtractor.extract(className)
         printer.println("$tag: [$threadName] $message")
     }
 }
