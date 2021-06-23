fdc28fb5d55f064c3f8c8ff90a465b7e8e6f644f
barakgavyus <58682332+bgavyus@users.noreply.github.com>
2021-06-23 20:27:19 +0300

    Simplify SurfaceDuplicator

--- app/src/main/java/io/github/bgavyus/lightningcamera/graphics/SurfaceDuplicator.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/graphics/SurfaceDuplicator.kt
@@ -1,6 +1,5 @@
 package io.github.bgavyus.lightningcamera.graphics
 
-import android.annotation.SuppressLint
 import android.graphics.SurfaceTexture
 import android.os.Handler
 import android.util.Size
@@ -20,55 +19,50 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 class SurfaceDuplicator(
-    private val handler: Handler,
+    handler: Handler,
     bufferSize: Size,
     surfaces: Iterable<Surface>,
 ) : DeferScope() {
-    private val scope = CoroutineScope(handler.asCoroutineDispatcher())
-        .apply { defer(::cancel) }
+    val surface = run {
+        val scope = CoroutineScope(handler.asCoroutineDispatcher())
+            .apply { defer(::cancel) }
 
-    private val core = EglCore(flags = EglCore.FLAG_TRY_GLES3)
-        .apply { defer(::release) }
+        val core = EglCore(flags = EglCore.FLAG_TRY_GLES3)
+            .apply { defer(::release) }
 
-    private val windows = surfaces
-        .map { EglWindowSurface(core, it) }
-        .onEach { defer(it::release) }
-        .apply { first().makeCurrent() }
+        val windows = surfaces
+            .map { EglWindowSurface(core, it) }
+            .onEach { defer(it::release) }
+            .apply { first().makeCurrent() }
 
-    private val texture = GlTexture()
+        val texture = GlTexture()
 
-    private val program = GlTextureProgram()
-        .apply { defer(::release) }
-        .also { it.texture = texture }
+        val program = GlTextureProgram()
+            .apply { defer(::release) }
+            .also { it.texture = texture }
 
-    private val entireViewport = GlRect()
-        .apply { defer(::release) }
+        val entireViewport = GlRect()
+            .apply { defer(::release) }
 
-    val surface: Surface
-
-    init {
-        @SuppressLint("Recycle")
         val surfaceTexture = SurfaceTexture(texture.id).apply {
             defer(::release)
             setDefaultBufferSize(bufferSize)
 
             frames(handler)
-                .onEach(::onFrameAvailable)
+                .onEach { surfaceTexture ->
+                    surfaceTexture.updateTexImage()
+                    surfaceTexture.getTransformMatrix(program.textureTransform)
+
+                    windows.forEach { window ->
+                        window.makeCurrent()
+                        window.setPresentationTime(surfaceTexture.timestamp)
+                        program.draw(entireViewport)
+                        window.swapBuffers()
+                    }
+                }
                 .launchIn(scope)
         }
 
-        surface = Surface(surfaceTexture)
-    }
-
-    private fun onFrameAvailable(surfaceTexture: SurfaceTexture) {
-        surfaceTexture.updateTexImage()
-        surfaceTexture.getTransformMatrix(program.textureTransform)
-
-        windows.forEach { window ->
-            window.makeCurrent()
-            window.setPresentationTime(surfaceTexture.timestamp)
-            program.draw(entireViewport)
-            window.swapBuffers()
-        }
+        Surface(surfaceTexture)
     }
 }
