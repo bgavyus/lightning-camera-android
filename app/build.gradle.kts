a224f94ab96c1f0cb73516f75eb375ca6936a65b
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-07-01 13:52:30 +0300

    Upgrade androidx core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
 
     // https://developer.android.com/jetpack/androidx/releases/core
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
 
     // https://developer.android.com/jetpack/androidx/releases/fragment
     implementation("androidx.fragment:fragment-ktx:1.3.5")
