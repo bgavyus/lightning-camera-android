bd8c01e9cbf6dba0d44f2174346fe0a4ca23b34b
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-07-01 13:54:30 +0300

    Upgrade hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     val kotlinVersion by extra("1.5.0")
 
     // https://github.com/google/dagger/releases
-    val hiltVersion by extra("2.36")
+    val hiltVersion by extra("2.37")
 
     repositories {
         google()
