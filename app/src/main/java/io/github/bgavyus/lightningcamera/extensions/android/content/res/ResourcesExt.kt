9100cf029d421f47bac0d72cf167a509c4d395e5
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-07-02 13:41:18 +0300

    ResourcesExt: simplify Resources.identifier

--- app/src/main/java/io/github/bgavyus/lightningcamera/extensions/android/content/res/ResourcesExt.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/extensions/android/content/res/ResourcesExt.kt
@@ -2,7 +2,5 @@ package io.github.bgavyus.lightningcamera.extensions.android.content.res
 
 import android.content.res.Resources
 
-fun Resources.identifier(name: String, defType: String? = null, defPackage: String? = null): Int? {
-    val id = getIdentifier(name, defType, defPackage)
-    return if (id == Resources.ID_NULL) null else id
-}
+fun Resources.identifier(name: String, defType: String? = null, defPackage: String? = null) =
+    getIdentifier(name, defType, defPackage).let { if (it == Resources.ID_NULL) null else it }
