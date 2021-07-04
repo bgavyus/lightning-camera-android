ad4c7966d598977c144deb8a3c4e143a7af7f352
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-07-04 13:05:41 +0300

    SamplesPool: thread safety

--- app/src/main/java/io/github/bgavyus/lightningcamera/media/SamplesPool.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/media/SamplesPool.kt
@@ -2,19 +2,15 @@ package io.github.bgavyus.lightningcamera.media
 
 import android.media.MediaCodec
 import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicInteger
 
 class SamplesPool(samplesCount: Int, sampleSize: Int) : Iterator<Sample> {
-    private var index = 0
+    private val index = AtomicInteger()
 
     private val array = Array(samplesCount) {
         Sample(ByteBuffer.allocateDirect(sampleSize), MediaCodec.BufferInfo())
     }
 
-    override fun next(): Sample {
-        val sample = array[index]
-        index = (index + 1) % array.size
-        return sample
-    }
-
+    override fun next() = array[index.getAndUpdate { (it + 1) % array.size }]
     override fun hasNext() = true
 }
