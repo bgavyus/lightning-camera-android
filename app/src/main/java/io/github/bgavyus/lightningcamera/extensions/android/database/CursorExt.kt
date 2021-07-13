8ac01d63738799e04ca9a515f9dccc92e98ee25e
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-07-13 17:40:14 +0300

    Simplified Cursor.get

--- app/src/main/java/io/github/bgavyus/lightningcamera/extensions/android/database/CursorExt.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/extensions/android/database/CursorExt.kt
@@ -4,17 +4,13 @@ import android.database.Cursor
 
 fun Cursor.requireMoveToPosition(position: Int) = check(moveToPosition(position))
 
-inline operator fun <reified T> Cursor.get(index: Int): T {
-    val value = when (T::class) {
-        ByteArray::class -> getBlob(index)
-        String::class -> getString(index)
-        Short::class -> getShort(index)
-        Int::class -> getInt(index)
-        Long::class -> getLong(index)
-        Float::class -> getFloat(index)
-        Double::class -> getDouble(index)
-        else -> throw IllegalArgumentException()
-    }
-
-    return value as T
-}
+inline operator fun <reified T> Cursor.get(index: Int) = when (T::class) {
+    ByteArray::class -> getBlob(index)
+    String::class -> getString(index)
+    Short::class -> getShort(index)
+    Int::class -> getInt(index)
+    Long::class -> getLong(index)
+    Float::class -> getFloat(index)
+    Double::class -> getDouble(index)
+    else -> throw IllegalArgumentException()
+} as T
