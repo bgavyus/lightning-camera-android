9c72ff19efdfc4f3e84be4f616aa4d6398d4c63b
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-09-25 20:24:16 +0300

    ApplicationTheme: set surface color to black

--- app/src/main/java/io/github/bgavyus/lightningcamera/ui/theme/ApplicationTheme.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/ui/theme/ApplicationTheme.kt
@@ -3,13 +3,16 @@ package io.github.bgavyus.lightningcamera.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun ApplicationTheme(
     content: @Composable () -> Unit,
 ) {
     MaterialTheme(
-        colors = darkColors(),
+        colors = darkColors(
+            surface = Color.Black,
+        ),
         content = content
     )
 }
