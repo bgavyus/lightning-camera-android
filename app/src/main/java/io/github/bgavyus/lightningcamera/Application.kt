6bd06fdf9780d29a08a1dfe2d0ef70de635ee3e4
bgavyus <58682332+bgavyus@users.noreply.github.com>
2021-09-25 20:23:34 +0300

    Application: add log when app starts

--- app/src/main/java/io/github/bgavyus/lightningcamera/Application.kt
+++ app/src/main/java/io/github/bgavyus/lightningcamera/Application.kt
@@ -3,11 +3,13 @@ package io.github.bgavyus.lightningcamera
 import android.app.Application
 import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
+import io.github.bgavyus.lightningcamera.logging.Logger
 
 @HiltAndroidApp
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        Logger.log("Created")
 
         if (BuildConfig.DEBUG) {
             enableStrictMode()
